c70173f12003131b619e547a2c8bc1dcde7673b9
maxime dufay <maxime.dufay@mobile-devices.fr>
2015-06-09 11:16:51 +0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :tasks
 
+  get '/auth/munic/callback', to: 'sessions#create'
+  #get '/auth/munic/callback', to: 'sessions#create_with_omniauth'      # if you already have your own session creation.
+  get '/signout', to: 'sessions#destroy'
+
   root 'tasks#index'
 end
